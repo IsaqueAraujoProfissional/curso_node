7db094cb40f46923ef882f5d5126a8f7e7b5b6c9
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2022-04-23 15:02:14 -0300

    Refatorando com Express

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.send("<html><body>Portal de Notícias</body></html>");
+});
+
+app.listen(3000, function(){
+    console.log("Servidor rodando com Express");
+});
