f0373a784fab438bb96b63478831484502ed5af9
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2022-04-23 15:20:41 -0300

    Instalando e testando Nodemon

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
     res.render("home/index");
 });
 
-app.get('/formulario_incluisao_noticia', function(req, res){
+app.get('/formulario_inclusao_noticia', function(req, res){
     res.render("admin/form_add_noticia");
 });
 app.get('/noticias', function(req, res){
