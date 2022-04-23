572b6a00425d8f6c39ab12adb235ea98f2a7f882
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2022-04-23 15:08:05 -0300

    Instalando EJS

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
-
 var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/tecnologia', function(req, res){
+    res.render("secao/tecnologia");
+});
 
 app.get('/', function(req, res){
     res.send("<html><body>Portal de Notícias</body></html>");
